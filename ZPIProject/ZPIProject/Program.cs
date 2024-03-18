2bec50e00aaf45307ed66260b201dbec8fc9ccf8
JakubMatuszak240749 <jaqobgmplus@gmail.com>
2024-03-18 17:11:42 +0100

    Created Arithemtics Interfaces and class

--- ZPIProject/ZPIProject/Program.cs
+++ ZPIProject/ZPIProject/Program.cs
@@ -3,3 +3,28 @@ Console.WriteLine("Team: 5D\nDeveloper: PiotrJurek240693");
 Console.WriteLine("Team: 5D\nTester: Kacper Michalec 240751");
 Console.WriteLine("Team: 5D\nDevops (Developer): WeronikaKretowicz240749");
 Console.WriteLine("Team: 5D\nDeveloper: MateuszMagd");
+
+interface IArithmeticsAdd
+{
+    double Addition(double a, double b);
+}
+
+interface IArithmeticsDiff
+{
+    double Difference(double a, double b);
+}
+
+interface IArithmeticsMult
+{
+    double Multiplication(double a, double b);
+}
+
+interface IArithmeticsDiv
+{
+    double Division(double a, double b);
+}
+
+public class Arithmetics
+{
+
+}
