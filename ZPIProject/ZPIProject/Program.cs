1b21d368d0ecaf781280e6de476f26987f4a4984
KacperM-02 <240751@edu.p.lodz.pl>
2024-03-19 17:30:44 +0100

    Deleted unnecessary using System

--- ZPIProject/ZPIProject/Program.cs
+++ ZPIProject/ZPIProject/Program.cs
@@ -4,8 +4,6 @@ Console.WriteLine("Team: 5D\nTester: Kacper Michalec 240751");
 Console.WriteLine("Team: 5D\nDevops (Developer): WeronikaKretowicz240749");
 Console.WriteLine("Team: 5D\nDeveloper: MateuszMagd");
 
-using System;
-
 interface IArithmeticsAdd   // Piotr Jurek
 {
     double Addition(double a, double b);
