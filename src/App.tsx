e65229bdb585f5f964682f5fd250ce3ba00a6742
PiotrJurek240693 <240693@edu.p.lodz.pl>
2024-06-06 13:05:39 +0200

    Fixing app.tsx tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,33 @@ import { CurrencyProvider } from './contexts/CurrencyContext';
 
 const App: React.FC = () => {
   return (
+    <div className='m-0 p-0 bg-white font-sans text-gray_for_text h-screen'>
     <CurrencyProvider>
       <Router basename="/ZPI2023_IO1_5D">
-        <div className="app">
-          <header className="header">
-            <h2>GLOBE</h2>
-          </header>
-          <div className="main">
-            <Sidebar />
-            <div className="main-content">
-              <div className="image-container">
-                <img src={Flags} alt="Flags" />
-              </div>
-              <div className="page-content">
-                <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/session-analysis" element={<SessionAnalysis />} />
-                  <Route path="/statistical-measures" element={<StatisticalMeasures />} />
-                  <Route path="/change-distribution" element={<ChangeDistribution />} />
-                </Routes>
+          <div className="flex flex-col h-screen">
+            <header className="header">
+              <h2>GLOBE</h2>
+            </header>
+            <div className="main">
+              <Sidebar />
+              <div className="main-content">
+                <div className="image-container">
+                  <img src={Flags} alt="Flags" />
+                </div>
+                <div className="page-content">
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/session-analysis" element={<SessionAnalysis />} />
+                    <Route path="/statistical-measures" element={<StatisticalMeasures />} />
+                    <Route path="/change-distribution" element={<ChangeDistribution />} />
+                  </Routes>
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </Router>
     </CurrencyProvider>
+        </div>
   );
 };
 
