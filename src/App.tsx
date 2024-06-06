28dfd45c4e4a80519d1cae6ae6226b49092cf102
PiotrJurek240693 <240693@edu.p.lodz.pl>
2024-06-06 13:35:33 +0200

    End of tailwind fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,20 @@ import { CurrencyProvider } from './contexts/CurrencyContext';
 
 const App: React.FC = () => {
   return (
-    <div className='m-0 p-0 bg-white font-sans text-gray_for_text h-screen'>
+    <div className='m-0 p-0 bg-white font-sans text-default_text'>
     <CurrencyProvider>
       <Router basename="/ZPI2023_IO1_5D">
           <div className="flex flex-col h-screen">
-            <header className="header">
-              <h2>GLOBE</h2>
+            <header className="bg-white text-blue p-2 text-left text-2xl border-b border-gray_for_text">
+              <h2 className="m-2 font-bold">GLOBE</h2>
             </header>
-            <div className="main">
+            <div className="flex flex-1">
               <Sidebar />
-              <div className="main-content">
-                <div className="image-container">
-                  <img src={Flags} alt="Flags" />
+              <div className="flex flex-col flex-1 items-center">
+                <div className="w-full flex justify-center mb-5">
+                  <img className='max-w-full h-auto' src={Flags} alt="Flags" />
                 </div>
-                <div className="page-content">
+                <div className="w-fit m-0">
                   <Routes>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/session-analysis" element={<SessionAnalysis />} />
