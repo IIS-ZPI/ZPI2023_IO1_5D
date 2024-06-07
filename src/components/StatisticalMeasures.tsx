1b7c2c1c24ab2c3b897c3e72f00ab52880a5b0b0
KacperM-02 <240751@edu.p.lodz.pl>
2024-06-07 19:46:18 +0200

    Fixed problems

--- src/components/StatisticalMeasures.tsx
+++ src/components/StatisticalMeasures.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useCurrency } from '../contexts/CurrencyContext';
-import { Statistics, useStatistics } from '../contexts/StatisticsContext';
+import React, { useEffect, useState } from "react";
+import { useCurrency } from "../contexts/CurrencyContext";
+import { Statistics, useStatistics } from "../contexts/StatisticsContext";
 
 const StatisticalMeasures: React.FC = () => {
-  const {  selectedCurrency, setSelectedCurrency, currencies, loading, setLoading } = useCurrency();
+  const {
+    selectedCurrency,
+    setSelectedCurrency,
+    currencies,
+    loading,
+    setLoading,
+  } = useCurrency();
   const { getStatistics } = useStatistics();
 
   const getDefaultStartingDate = () => {
@@ -11,139 +17,221 @@ const StatisticalMeasures: React.FC = () => {
     const lastMonth = new Date(today.setMonth(today.getMonth() - 1));
 
     const year = lastMonth.getFullYear();
-    const month = String(lastMonth.getMonth() + 1).padStart(2, '0');
-    const day = String(lastMonth.getDate()).padStart(2, '0');
+    const month = String(lastMonth.getMonth() + 1).padStart(2, "0");
+    const day = String(lastMonth.getDate()).padStart(2, "0");
 
     return `${year}-${month}-${day}`;
   };
 
-  const getEndDate = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-
-    return `${year}-${month}-${day}`;
-  };
-
-  const [startingDate, setStartingDate] = useState(getDefaultStartingDate);
-  const [timePeriod, setTimePeriod] = useState(getEndDate);
-  const [endDate, setEndDate] = useState(getEndDate);
-
-
-  const [inputValue, setInputValue] = useState("");
+  const [startingDate, setStartingDate] = useState(() =>
+    getDefaultStartingDate()
+  );
+  const [isStartingDateSelected, setIsStartingDateSelected] = useState(false);
   const [statistics, setStatistics] = useState<Statistics | null>(null);
 
-  const handleCurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedCurrency(e.target.value);
-  };
+  const calculateEndDate = (startingDate: string, timePeriod: string) => {
+    const startDate = new Date(startingDate);
+    let result: Date;
 
-  const handleTimePeriodChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setStartingDate
-  };
-
-  const handleStartingDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value === "") {
-      setInputValue("");
-      setStartingDate(getDefaultStartingDate());
-    } else {
-      setInputValue(value);
-      setStartingDate(value);
+    switch (timePeriod) {
+      case "7 days":
+        result = new Date(startDate.setDate(startDate.getDate() + 7));
+        break;
+      case "30 days":
+      case "Switch period":
+        result = new Date(startDate.setMonth(startDate.getMonth() + 1));
+        break;
+      case "90 days":
+        result = new Date(startDate.setMonth(startDate.getMonth() + 3));
+        break;
+      default:
+        console.error("Error with timePeriod");
+        return "";
     }
+
+    const year = result.getFullYear();
+    const month = String(result.getMonth() + 1).padStart(2, "0");
+    const day = String(result.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
   };
+
+  const [timePeriod, setTimePeriod] = useState("Switch period");
+  const [isTimePeriodSelected, setIsTimePeriodSelected] = useState(false);
+  const [endDate, setEndDate] = useState(() =>
+    calculateEndDate(startingDate, timePeriod)
+  );
 
   useEffect(() => {
+    if (isStartingDateSelected != isTimePeriodSelected) return;
     const fetchStatistics = async () => {
       setLoading(true);
       try {
-        const stats = await getStatistics(selectedCurrency, startingDate, endDate);
+        const stats = await getStatistics(
+          selectedCurrency,
+          startingDate,
+          endDate
+        );
         setStatistics(stats);
       } catch (error) {
-        console.error('Error fetching statistics:', error);
+        console.error("Error fetching statistics:", error);
       } finally {
         setLoading(false);
       }
     };
 
     fetchStatistics();
-  }, [selectedCurrency, getStatistics]);
+  }, [selectedCurrency, timePeriod]);
 
+  const handleCurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCurrency(e.target.value);
+  };
+
+  const handleStartingDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newStartingDate = e.target.value;
+    if (newStartingDate === "") {
+      setStartingDate(getDefaultStartingDate());
+      setIsStartingDateSelected(false);
+
+      setTimePeriod("Switch period");
+      setIsTimePeriodSelected(false);
+      setEndDate(calculateEndDate(getDefaultStartingDate(), "Switch period"));
+      return;
+    }
+
+    setStartingDate(newStartingDate);
+    setIsStartingDateSelected(true);
+
+    setTimePeriod("Switch period");
+    setIsTimePeriodSelected(false);
+    setStatistics(null);
+    setEndDate("");
+  };
+
+  const handleTimePeriodChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newTimePeriod = e.target.value;
+    setTimePeriod(newTimePeriod);
+    setIsTimePeriodSelected(true);
+    setEndDate(calculateEndDate(startingDate, newTimePeriod));
+  };
+
+  const daysDifference = (days: number) => {
+    if (!isStartingDateSelected) return false;
+    const today = new Date();
+    const startDate = new Date(startingDate);
+    const differenceInTime = today.getTime() - startDate.getTime();
+    const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+    return differenceInDays >= days;
+  };
+
+  const getMaxDate = () => {
+    const today = new Date();
+    const maxDate = new Date(today.setDate(today.getDate() - 7));
+    const year = maxDate.getFullYear();
+    const month = String(maxDate.getMonth() + 1).padStart(2, "0");
+    const day = String(maxDate.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  if (!statistics) {
-    return <p>No statistical data available.</p>;
-  }
-
   return (
-    <div className='size-fit' >
+    <div className="w-full">
       <div className="flex justify-between items-center mb-8 border-b border-gray_for_text">
-        <h1 className="text-2xl font-bold">{selectedCurrency}</h1>
-        <div className="text-xl font-medium">{startingDate} - {endDate}</div>
-      </div>
-      <div className="flex justify-between mb-8">
-        <div className="relative w-64">
-        <label htmlFor="currency" className="block text-gray-700 text-sm font-bold mb-2">
-          Currency
-        </label>
-        <div className="relative">
-        {loading ? (
-          <p>Loading currencies...</p>
-        ) : (
-          <>
-            <select
-              id="currency"
-              className="h-10 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              value={selectedCurrency} onChange={handleCurrencyChange}>
-              {
-                currencies.map((currency) => (
-                <option key={currency.code} value={currency.code}>
-                  {currency.code} - {currency.name}
-                </option>))
-              }
-            </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path d="M7 10l5 5 5-5H7z" />
-                </svg>
-              </div>
-          </>
-        )}
+        <h1 className="text-3xl font-bold">{selectedCurrency}</h1>
+        <div className="text-xl font-bold">
+          {startingDate} - {endDate}
         </div>
+      </div>
+      <div className="flex mb-8">
+        <div className="relative w-64">
+          <label
+            htmlFor="currency"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Currency
+          </label>
+          <div className="relative">
+            {loading ? (
+              <p>Loading currencies...</p>
+            ) : (
+              <>
+                <select
+                  id="currency"
+                  className="h-10 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  value={selectedCurrency}
+                  onChange={handleCurrencyChange}
+                >
+                  {currencies.map((currency) => (
+                    <option key={currency.code} value={currency.code}>
+                      {currency.code} - {currency.name}
+                    </option>
+                  ))}
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M7 10l5 5 5-5H7z" />
+                  </svg>
+                </div>
+              </>
+            )}
+          </div>
         </div>
         <div className="flex">
           <div className="relative w-64">
-            <label htmlFor="start-date" className="block text-gray-700 text-sm font-bold mb-2">
+            <label
+              htmlFor="start-date"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Starting date
             </label>
             <input
-            type="date"
-            id="start-date"
-            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-            value={inputValue}
-            onChange={handleStartingDateChange}
-            placeholder="Select a date"/>
+              type="date"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleStartingDateChange}
+              placeholder="Select a date"
+              value={isStartingDateSelected ? startingDate : ""}
+              max={getMaxDate()}
+            />
           </div>
-          <div className="relative w-64">
-            <label htmlFor="time-period" className="block text-gray-700 text-sm font-bold mb-2">
+          <div className="relative w-64 pl-3">
+            <label
+              htmlFor="time-period"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Time period
             </label>
             <div className="relative">
               <select
-                id="time-period"
                 className="h-10 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 onChange={handleTimePeriodChange}
+                value={isTimePeriodSelected ? timePeriod : "Switch period"}
               >
-                <option>Switch period</option>
-                <option>7 days</option>
-                <option>30 days</option>
-                <option>90 days</option>
+                <option disabled selected hidden>
+                  Switch period
+                </option>
+                <option value="7 days" disabled={!daysDifference(7)}>
+                  7 days
+                </option>
+                <option value="30 days" disabled={!daysDifference(30)}>
+                  30 days
+                </option>
+                <option value="90 days" disabled={!daysDifference(90)}>
+                  90 days
+                </option>
               </select>
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
                   <path d="M7 10l5 5 5-5H7z" />
                 </svg>
               </div>
@@ -153,8 +241,8 @@ const StatisticalMeasures: React.FC = () => {
       </div>
       <div className="grid grid-cols-2 gap-4">
         <div>
-          <h2 className="text-xl font-bold">Parameters</h2>
-          <div className="mt-4">
+          <h2 className="text-2xl font-bold">Parameters</h2>
+          <div className="mt-4 font-medium text-xl">
             <p className="mb-2">Mode:</p>
             <p className="mb-2">Median:</p>
             <p className="mb-2">Standard deviation:</p>
@@ -162,31 +250,31 @@ const StatisticalMeasures: React.FC = () => {
           </div>
         </div>
         <div>
-          <h2 className="text-xl font-bold">Values</h2>
-          <div className="mt-4">
+          <h2 className="text-2xl font-bold">Values</h2>
+          <div className="mt-4 font-medium text-xl">
             <input
               type="text"
               readOnly
-              className="mb-2 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={statistics.mode}
+              className="mb-2 block rounded border border-gray-300 auto-width-input"
+              value={statistics ? statistics.mode : 0}
             />
             <input
               type="text"
               readOnly
-              className="mb-2 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={statistics.median}
+              className="mb-2 block rounded border border-gray-300 auto-width-input"
+              value={statistics ? statistics.median : 0}
             />
             <input
               type="text"
               readOnly
-              className="mb-2 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={statistics.standardDeviation}
+              className="mb-2 block rounded border border-gray-300 auto-width-input"
+              value={statistics ? statistics.standardDeviation : 0}
             />
             <input
               type="text"
               readOnly
-              className="mb-2 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={statistics.coefficientOfVariation}
+              className="mb-2 block rounded border border-gray-300 auto-width-input"
+              value={statistics ? statistics.coefficientOfVariation : 0}
             />
           </div>
         </div>
